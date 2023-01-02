2cd538f758e3bb3638e3af28286be9869ebe0750
pelmegov <pelmegovsa@gmail.com>
2023-01-02 16:32:03 +0100

    implemented 2 charts

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import {useStateContext} from "../../contexts/ContextProvider";
+import {
+    ChartComponent,
+    DateTime,
+    Inject,
+    Legend,
+    SplineAreaSeries,
+    SeriesCollectionDirective,
+    SeriesDirective
+} from "@syncfusion/ej2-react-charts";
+import {areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis} from "../../data/dummy";
+import { Header } from "../../components";
 
 function Area(props) {
+    const {currentMode} = useStateContext();
+
     return (
-        <div>Area</div>
+        <div>
+            <Header category="Area" title="Inflation Rate in Percentage"/>
+
+            <ChartComponent
+                id="area-char"
+                height="420px"
+                primaryXAxis={areaPrimaryXAxis}
+                primaryYAxis={areaPrimaryYAxis}
+                chartArea={{ border: {width: 0}}}
+                tooltip={{ enable: true }}
+                background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+            >
+                <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+                <SeriesCollectionDirective>
+                    {areaCustomSeries.map((item, index) =>
+                        <SeriesDirective key={index} {...item} />
+                    )}
+                </SeriesCollectionDirective>
+            </ChartComponent>
+        </div>
     );
 }
 
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Header, LineChart} from "../../components";
 
 function Line(props) {
     return (
-        <div>Line</div>
+        <div className="mt-4 md:mt-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+            <Header category="Chart" title="Inflation Rate"/>
+            <div className="w-full">
+                <LineChart />
+            </div>
+        </div>
     );
 }
 
